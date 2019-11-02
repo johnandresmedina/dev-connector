4e86f310bcfe251caea03f67ba90708ac14e1ed5
John Medina <jhonandresmedina@gmail.com>
2019-11-02 18:07:54 -0500

    adding database connection

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+connectDB();
 
 app.get("/", (request, response) => response.send("API Running"));
 

625a8d6b1d4edc854791cef74f3a0760561804dd
John Medina <jhonandresmedina@gmail.com>
2019-11-04 18:54:56 -0500

    adding user login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
@@ -17,5 +21,59 @@ router.get("/", auth, async (request, response) => {
     response.status(500).send("Server error");
   }
 });
+
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists()
+  ],
+  async (request, response) => {
+    const errors = validationResult(request);
+
+    if (!errors.isEmpty()) {
+      return response.status(400).json({ error: errors.array() });
+    }
+
+    const { email, password } = request.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return response
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return response
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (error, token) => {
+          if (error) throw error;
+          response.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(err.message);
+      response.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+
 const User = require("../../models/User");
 
 const router = express.Router();
